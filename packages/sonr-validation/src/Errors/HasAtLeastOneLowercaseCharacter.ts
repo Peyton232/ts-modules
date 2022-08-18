bd96235592a16586fc49085be36e78d56cda033c
Stéfano Damiano <stefano@ae.studio>
2022-08-18 18:12:31 -0300

    fix: setting error messages properly and fixing validator to read message field (#39)

--- packages/sonr-validation/src/Errors/HasAtLeastOneLowercaseCharacter.ts
+++ packages/sonr-validation/src/Errors/HasAtLeastOneLowercaseCharacter.ts
@@ -1,7 +1,8 @@
 export class ErrorHasAtLeastOneLowercaseCharacter extends Error {
-    private static _message: 'Should have at least one lowercase character.';
-    
-    constructor() {
-        super(ErrorHasAtLeastOneLowercaseCharacter._message);
-    }
+	private static _message: string =
+		"Should have at least one lowercase character."
+
+	constructor() {
+		super(ErrorHasAtLeastOneLowercaseCharacter._message)
+	}
 }
